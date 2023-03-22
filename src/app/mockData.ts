cca6abdfb9f914d898ba7de2eecbc11a9629f3d0
Naveen <naveenaithal44@gmail.com>
2023-03-22 19:28:20 +0530

    minor changes

--- src/app/mockData.ts
+++ src/app/mockData.ts
@@ -237,3 +237,106 @@ export const weeklyTrip=  [
     totalCount: 545
   }
 ]
+
+export const mockData = [
+      {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-20T08:44"
+    },
+      {
+        "from": "zone1",
+        "to": "zone2",
+        "dateTime": "2023-03-20T09:30"
+    },
+      {
+        "from": "zone2",
+        "to": "zone2",
+        "dateTime": "2023-03-20T17:40"
+    },
+      {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-20T18:20"
+    },
+      {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-20T08:20"
+    },
+    {
+        "from": "zone1",
+        "to": "zone2",
+        "dateTime": "2023-03-21T09:46"
+    },
+       {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-21T10:00"
+    },
+       {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-21T10:00"
+    },
+       {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-21T10:00"
+    },
+       {
+        "from": "zone2",
+        "to": "zone1",
+        "dateTime": "2023-03-21T10:00"
+    },
+       {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-21T17:50"
+    },
+       {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-21T18:46"
+    },
+       {
+        "from": "zone1",
+        "to": "zone2",
+        "dateTime": "2023-03-22T11:47"
+    },
+    {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-23T10:47"
+    },
+    {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-24T17:47"
+    },
+    {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-24T17:47"
+    },
+    {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-24T17:47"
+    },
+    {
+        "from": "zone1",
+        "to": "zone1",
+        "dateTime": "2023-03-24T17:47"
+    },
+     {
+        "from": "zone2",
+        "to": "zone2",
+        "dateTime": "2023-03-25T18:12"
+    },
+      {
+        "from": "zone1",
+        "to": "zone2",
+        "dateTime": "2023-03-26T09:12"
+    }
+]
